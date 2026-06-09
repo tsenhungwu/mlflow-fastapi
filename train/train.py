152b83ec26f3936eb58a1e5aa17a11794d7d8515
Tsen-Hung Wu <tsenhungwu@Tsen-HungdeMacBook-Pro.local>
2026-06-09 13:50:42 +0800

    formatted train.py

--- train/train.py
+++ train/train.py
@@ -1,11 +1,13 @@
 import os
+
 import mlflow
 import mlflow.sklearn
-
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 
-mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI", "http://localhost:5001"))
+mlflow.set_tracking_uri(
+    os.getenv("MLFLOW_TRACKING_URI", "http://localhost:5001")
+)
 
 X, y = load_iris(return_X_y=True)
 
@@ -15,8 +17,6 @@ model.fit(X, y)
 
 with mlflow.start_run() as run:
     mlflow.sklearn.log_model(
-        model,
-        "model",
-        registered_model_name="iris_model"
+        model, "model", registered_model_name="iris_model"
     )
     print(f"RUN_ID={run.info.run_id}")
